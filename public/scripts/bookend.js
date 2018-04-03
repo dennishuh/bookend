f4d18436482e9f47c1152a0c5c6643249d2e8d0a
Dennis Huh <denjhuh@gmail.com>
2018-04-03 14:38:38 -0500

    Added in alert slideup

--- public/scripts/bookend.js
+++ public/scripts/bookend.js
@@ -1,19 +1,3 @@
-// const $searchResults = $('.search-results');
-//
-// $searchResults.on('click', '.start-reading-btn', function(e) {
-//   const btn = this;
-//   const bookInfo = $(this).data('bookInfo');
-//   $.ajax({
-//     type: 'POST',
-//     url: '/books/add',
-//     data: bookInfo,
-//     dataType: 'json',
-//     success: function(res) {
-//       if (res.added) {
-//         console.log('book added');
-//         console.log(this);
-//         $(btn).closest('.book-viewer').addClass('book-saved');
-//       }
-//     }
-//   });
-// })
+$(".alert-dismissible").fadeTo(2000, 500).slideUp(500, function(){
+    $(".alert-dismissible").slideUp(500);
+});
